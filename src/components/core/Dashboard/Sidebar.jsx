4e8e71b5b96049b5261df32797432038ea662adf
Lokesh <lokesh1992p@gmail.com>
2026-09-20 23:00:40 +0530

    refactor: Sidebar logout and user moved to the auth store
    Reads the user and the loading state from the store, and confirms logout
    through the same mutation as the rest of the app.

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { sidebarLinks } from '../../../data/dashboard-links'
 import SidebarLink from './SidebarLink';
 import { VscSignOut } from 'react-icons/vsc';
 import { useNavigate } from 'react-router-dom';
 import ConfirmationModal from '../../common/ConfirmationModal';
-import { logout } from "../../../services/operations/authAPI"
+import { useLogout } from "@/hooks/use-auth-query"
 
 const Sidebar = () => {
     const { user, loading: profileLoading } = useSelector((state) => state.profile);
     const { loading: authLoading } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
+    const { mutate: logout } = useLogout();
     const navigate = useNavigate();
 
     const [confirmationModal, setConfirmationModal] = useState(null)
@@ -54,7 +54,7 @@ const Sidebar = () => {
                                 text2: "You will be logged out of your account.",
                                 btn1Text: "Logout",
                                 btn2Text: "Cancel",
-                                btn1Handler: () => dispatch(logout(navigate)),
+                                btn1Handler: () => logout(undefined, { onSettled: () => navigate("/") }),
                                 btn2Handler: () => setConfirmationModal(null),
                             })
                         }
